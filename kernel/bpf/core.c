0a2068963ccddf53c6ecbc4f9f6dfebeadfb6e78
Martin KaFai Lau <kafai@fb.com>
2016-12-07 15:53:11 -0800

    bpf: xdp: Allow head adjustment in XDP prog
    This patch allows XDP prog to extend/remove the packet
    data at the head (like adding or removing header).  It is
    done by adding a new XDP helper bpf_xdp_adjust_head().
    
    It also renames bpf_helper_changes_skb_data() to
    bpf_helper_changes_pkt_data() to better reflect
    that XDP prog does not work on skb.
    
    This patch adds one "xdp_adjust_head" bit to bpf_prog for the
    XDP-capable driver to check if the XDP prog requires
    bpf_xdp_adjust_head() support.  The driver can then decide
    to error out during XDP_SETUP_PROG.
    
    [huexxx@gmail.com: Drop powerpc and mellanox changes]
    Change-Id: Id014784cbd7cb6afa63ab9ac667f9804c45f34e1
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: John Fastabend <john.r.fastabend@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -1282,6 +1282,15 @@ struct bpf_prog * __weak bpf_int_jit_compile(struct bpf_prog *prog)
 }
 
 bool __weak bpf_helper_changes_skb_data(void *func)
+||||||| parent of d9e394de01d6 (bpf: xdp: Allow head adjustment in XDP prog)
+/* Stub for JITs that support eBPF. All cBPF code gets transformed into
+ * eBPF by the kernel and is later compiled by bpf_int_jit_compile().
+ */
+void __weak bpf_jit_compile(struct bpf_prog *prog)
+{
+}
+
+bool __weak bpf_helper_changes_pkt_data(void *func)
 {
 	return false;
 }
