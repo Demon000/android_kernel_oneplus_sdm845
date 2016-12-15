89115a14afef44c0d42d321102beea483e35ccd0
Daniel Mack <daniel@zonque.org>
2016-12-15 10:53:21 +0100

    bpf: cgroup: annotate pointers in struct cgroup_bpf with __rcu
    The member 'effective' in 'struct cgroup_bpf' is protected by RCU.
    Annotate it accordingly to squelch a sparse warning.
    
    Change-Id: I5d3cebeb5870bef719e276e276e9eac0af0cf4a6
    Signed-off-by: Daniel Mack <daniel@zonque.org>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/bpf-cgroup.h
+++ include/linux/bpf-cgroup.h
@@ -20,7 +20,7 @@ struct cgroup_bpf {
 	 * when this cgroup is accessed.
 	 */
 	struct bpf_prog *prog[MAX_BPF_ATTACH_TYPE];
-	struct bpf_prog *effective[MAX_BPF_ATTACH_TYPE];
+	struct bpf_prog __rcu *effective[MAX_BPF_ATTACH_TYPE];
 	bool disallow_override[MAX_BPF_ATTACH_TYPE];
 };
 
