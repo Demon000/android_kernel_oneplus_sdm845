47a2ffce53591f3a04c9ca32d2f276d512fb6ea9
Cosmin Tanislav <demonsingur@gmail.com>
2025-07-09 01:09:45 +0300

    fixup! ipv6: implement dataplane support for rthdr type 4 (Segment Routing Header)

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -290,13 +290,9 @@ static struct ipv6_devconf ipv6_devconf_dflt __read_mostly = {
 	.use_oif_addrs_only	= 0,
 	.ignore_routes_with_linkdown = 0,
 	.keep_addr_on_down	= 0,
-<<<<<<< HEAD
 	.accept_ra_prefix_route = 1,
 	.addr_gen_mode		= IN6_ADDR_GEN_MODE_EUI64,
-||||||| parent of 1ababeba4a21 (ipv6: implement dataplane support for rthdr type 4 (Segment Routing Header))
-=======
 	.seg6_enabled		= 0,
->>>>>>> 1ababeba4a21 (ipv6: implement dataplane support for rthdr type 4 (Segment Routing Header))
 };
 
 /* Check if link is ready: is it up and is a valid qdisc available */
@@ -5039,12 +5035,8 @@ static inline void ipv6_store_devconf(struct ipv6_devconf *cnf,
 	array[DEVCONF_DROP_UNICAST_IN_L2_MULTICAST] = cnf->drop_unicast_in_l2_multicast;
 	array[DEVCONF_DROP_UNSOLICITED_NA] = cnf->drop_unsolicited_na;
 	array[DEVCONF_KEEP_ADDR_ON_DOWN] = cnf->keep_addr_on_down;
-<<<<<<< HEAD
 	array[DEVCONF_ADDR_GEN_MODE] = cnf->addr_gen_mode;
-||||||| parent of 1ababeba4a21 (ipv6: implement dataplane support for rthdr type 4 (Segment Routing Header))
-=======
 	array[DEVCONF_SEG6_ENABLED] = cnf->seg6_enabled;
->>>>>>> 1ababeba4a21 (ipv6: implement dataplane support for rthdr type 4 (Segment Routing Header))
 }
 
 static inline size_t inet6_ifla6_size(void)
