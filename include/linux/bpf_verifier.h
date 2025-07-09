d683d387de7e9c73014e86be366e06223a1c1c46
Cosmin Tanislav <demonsingur@gmail.com>
2025-07-09 13:43:17 +0300

    Revert "bpf: fix branch pruning logic"
    This reverts commit 7b5b73ea87a06236fa124bdebed1390d362d3439.
    
    Change-Id: I75bbcdfaba17d991767010c856ca474b1fd75e50

--- include/linux/bpf_verifier.h
+++ include/linux/bpf_verifier.h
@@ -71,7 +71,6 @@ struct bpf_insn_aux_data {
 		struct bpf_map *map_ptr;        /* pointer for call insn into lookup_elem */
 	};
 	int sanitize_stack_off; /* stack slot to be cleared */
-	bool seen; /* this insn was processed by the verifier */
 };
 
 #define MAX_USED_MAPS 64 /* max number of maps accessed by one eBPF program */
