57aac373cbe18ad7f50d17883cc43c19573c13b5
Cosmin Tanislav <demonsingur@gmail.com>
2025-07-09 20:12:41 +0300

    fixup! UPSTREAM: selinux: bpf: Add addtional check for bpf object file receive

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -329,6 +329,9 @@ _out:							\
 #ifdef CONFIG_BPF_SYSCALL
 DECLARE_PER_CPU(int, bpf_prog_active);
 
+extern const struct file_operations bpf_map_fops;
+extern const struct file_operations bpf_prog_fops;
+
 #define BPF_PROG_TYPE(_id, _ops) \
 	extern const struct bpf_verifier_ops _ops;
 #define BPF_MAP_TYPE(_id, _ops) \
@@ -336,9 +339,6 @@ DECLARE_PER_CPU(int, bpf_prog_active);
 #include <linux/bpf_types.h>
 #undef BPF_PROG_TYPE
 #undef BPF_MAP_TYPE
-
-extern const struct file_operations bpf_map_fops;
-extern const struct file_operations bpf_prog_fops;
 
 extern const struct bpf_verifier_ops bpf_offload_prog_ops;
 
