651e57c7222d1947a48d74967fdd0b361d984dcb
Cosmin Tanislav <demonsingur@gmail.com>
2025-07-12 14:48:24 +0300

    Revert "vfs: Add current_time() api"
    This reverts commit 63f3641b22cf71e8336dee5e9eb9d8031013e7af.

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1511,8 +1511,6 @@ static inline void i_gid_write(struct inode *inode, gid_t gid)
 extern struct timespec current_fs_time(struct super_block *sb);
 extern struct timespec current_time(struct inode *inode);
 
-extern struct timespec current_time(struct inode *inode);
-
 /*
  * Snapshotting support.
  */
