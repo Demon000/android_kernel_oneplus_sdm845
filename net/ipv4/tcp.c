d637597a37fdd8eea6391ebb7f85c22c59a184de
Cosmin Tanislav <demonsingur@gmail.com>
2025-07-12 14:00:24 +0300

    fixup! fixup! tcp: SOF_TIMESTAMPING_OPT_STATS option for SO_TIMESTAMPING

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -3092,9 +3092,12 @@ struct sk_buff *tcp_get_timestamping_opt_stats(const struct sock *sk)
 		return NULL;
 
 	tcp_get_info_chrono_stats(tp, &info);
-	nla_put_u64_64bit(stats, TCP_NLA_BUSY, info.tcpi_busy_time);
-	nla_put_u64_64bit(stats, TCP_NLA_RWND_LIMITED, info.tcpi_rwnd_limited);
-	nla_put_u64_64bit(stats, TCP_NLA_SNDBUF_LIMITED, info.tcpi_sndbuf_limited);
+	nla_put_u64_64bit(stats, TCP_NLA_BUSY,
+			  info.tcpi_busy_time, TCP_NLA_PAD);
+	nla_put_u64_64bit(stats, TCP_NLA_RWND_LIMITED,
+			  info.tcpi_rwnd_limited, TCP_NLA_PAD);
+	nla_put_u64_64bit(stats, TCP_NLA_SNDBUF_LIMITED,
+			  info.tcpi_sndbuf_limited, TCP_NLA_PAD);
 	return stats;
 }
 
