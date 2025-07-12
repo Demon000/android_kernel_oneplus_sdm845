3154244b7802020a7dc5a6fc8ac8bd712361a9ef
Cosmin Tanislav <demonsingur@gmail.com>
2025-07-12 13:57:50 +0300

    fixup! bpf: use different interpreter depending on required stack size

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -1595,7 +1595,7 @@ STACK_FRAME_NON_STANDARD(___bpf_prog_run); /* jump table */
 
 #define PROG_NAME(stack_size) __bpf_prog_run##stack_size
 #define DEFINE_BPF_PROG_RUN(stack_size) \
-static unsigned int PROG_NAME(stack_size)(const struct sk_buff *ctx, const struct bpf_insn *insn) \
+static unsigned int PROG_NAME(stack_size)(const void *ctx, const struct bpf_insn *insn) \
 { \
 	u64 stack[stack_size / sizeof(u64)]; \
 	u64 regs[MAX_BPF_EXT_REG]; \
